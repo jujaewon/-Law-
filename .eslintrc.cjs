c0c13037dac8d3d8d4c5c50d7fc5e2709bc63062
이가영 <tp06100@naver.com>
2024-04-22 17:37:33 +0900

    Feat : Strorybook 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true, jest: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:import/typescript', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['vite.config.ts', 'dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
