b4506c868e6ebe4d477bc3ac913ba8df715d1e01
dla612 <dla612@naver.com>
2024-04-24 17:41:19 +0900

    feat: 로그인 기능 추가

---
+++ back-auth/src/main/java/com/hellolaw/auth/service/UserService.java
@@ -0,0 +1,10 @@
+package com.hellolaw.auth.service;
+
+import com.hellolaw.auth.dto.TokenResponse;
+import com.hellolaw.auth.dto.UserInfoResponse;
+import com.hellolaw.auth.provider.AuthProvider;
+
+public interface UserService {
+
+	TokenResponse login(UserInfoResponse userInfoResponse, AuthProvider authProvider);
+}
