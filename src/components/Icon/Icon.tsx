db20b77ed0c074f822b5f71af643483f58749559
이가영 <tp06100@naver.com>
2024-04-24 17:59:14 +0900

    fix : Icon 컴포넌트 설정 변경 -  string 변수로도 사용할수있게 iconType에 없는 string일 경우 default값 설정

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -5,12 +5,15 @@ export const iconTypes: IconType[] = Object.keys(icons) as any[];
 
 export type IconProps = {
   /** 아이콘 타입 설정 */
-  icon: IconType;
+  icon: IconType | string;
   className?: string;
 };
 
 const Icon = ({ icon, className }: IconProps) => {
-  const SVGIcon = icons[icon];
+  const SVGIcon = (icons as any)[icon] || icons['test'];
+  if (!SVGIcon) {
+    return null;
+  }
   return <SVGIcon className={className} />;
 };
 
