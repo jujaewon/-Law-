3c7c51ddac63c798afdc5872037315ebf7f1718c
최이초 <choeyicho1105@gmail.com>
2024-04-25 12:59:01 +0900

    init: 초기 설정

---
+++ hellolaw/src/main/java/com/hellolaw/hellolaw/HellolawApplication.java
@@ -0,0 +1,13 @@
+package com.hellolaw.hellolaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellolawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellolawApplication.class, args);
+	}
+
+}
