54dcad295487bfa47c603079f0141d33dcb9e811
이가영 <tp06100@naver.com>
2024-04-26 12:56:09 +0900

    fix  : 모달 props 변경

--- src/components/Modal/manage/CreatePortal.tsx
+++ src/components/Modal/manage/CreatePortal.tsx
@@ -9,27 +9,28 @@ function CreatePortal() {
   const openedModals = useContext(ModalsStateContext);
   const { closeModal } = useModal();
   const modalElement = document.getElementById('root-modal');
-
   // openedModals state를 map으로 순회하면서
   // 렌더링할 모달컴포넌트를 리턴하고, createPortal에 전달한다.
   const renderModal = openedModals.map((modal, index) => {
     const { type, props, isOpen } = modal;
     const ModalComponent = type && MODAL_COMPONENTS[type];
-    const { onSubmit, onClose, ...rest } = props;
-    if (!isOpen || !props) return null;
+
+    if (!props || !isOpen) return null;
+    const { onSubmit, ...rest } = props;
+
+    const onClose = () => {
+      closeModal(type);
+    };
 
     const handleSubmit = async () => {
       if (typeof onSubmit === 'function') {
         await onSubmit();
       }
-    };
-    const handleClick = () => {
-      onClose && onClose();
-      closeModal(type);
+      onClose();
     };
 
     // 각 모달 컴포넌트에 필요한 props 전달
-    return <ModalComponent key={index} isOpen={isOpen} onClose={handleClick} onSubmit={handleSubmit} {...rest} />;
+    return <ModalComponent key={index} isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit} {...rest} />;
   });
 
   // 모든 모달들을 포털을 통해 'root-modal' 요소로 렌더링
