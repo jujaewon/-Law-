16e3590c39f1957941edc7df2be4ba7312566a27
박우철 <woochul102@gmail.com>
2024-04-30 15:05:18 +0900

    feat: Test API

--- hellolaw/src/main/java/com/hellolaw/hellolaw/entity/TestControler.java
+++ hellolaw/src/main/java/com/hellolaw/hellolaw/entity/TestControler.java
@@ -1,14 +1,19 @@
 package com.hellolaw.hellolaw.entity;
 
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.hellolaw.hellolaw.entity.UserRepository;
+
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 public class TestControler {
 
+  private final UserRepository userRepository;
   @GetMapping("/test")
   public String test() {
     log.info("test");
@@ -16,7 +21,11 @@ public class TestControler {
     User user = User.builder()
       .nickname("test")
       .build();
-    log.info("user: {}", user);
+
+    userRepository.save(user);
+    User test = userRepository.findByNickname("test").orElseThrow();
+    log.info(test.getNickname());
+
 
     return "test";
   }
