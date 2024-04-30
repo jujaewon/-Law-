43888d801f25918518bf06825edf092d69b4aa62
박우철 <woochul102@gmail.com>
2024-04-30 16:40:05 +0900

    feat: TestController 추가

--- hellolaw/src/main/java/com/hellolaw/hellolaw/controller/TestController.java
+++ hellolaw/src/main/java/com/hellolaw/hellolaw/controller/TestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
   private final UserRepository userRepository;
-  @GetMapping("/test")
+  @GetMapping("/api/test")
   public String test() {
     log.info("test");
 
