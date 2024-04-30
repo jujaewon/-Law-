6483041f099c5645903c100a6ea00b64e98070b5
박우철 <woochul102@gmail.com>
2024-04-30 14:16:02 +0900

    feat: Test API

--- hellolaw/src/main/java/com/hellolaw/hellolaw/entity/TestControler.java
+++ hellolaw/src/main/java/com/hellolaw/hellolaw/entity/TestControler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestControler {
 
-  @GetMapping("/test")
+  @GetMapping("/api/test")
   public String test() {
     log.info("test");
 
