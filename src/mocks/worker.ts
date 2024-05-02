c1200bf7d30847b4e7ff54178c4f7da3926e34c1
이가영 <tp06100@naver.com>
2024-05-02 17:27:38 +0900

    fix : msw node not find 오류 수정중

--- src/mocks/worker.ts
+++ src/mocks/worker.ts
@@ -1,10 +1,14 @@
 async function initMSW() {
   if (typeof window === 'undefined') {
-    const { server } = await import('./server');
-    server.listen(); // Node.js 환경에서 Server 활성화
+    // const { server } = await import('./server');
+    // server.listen({
+    //   onUnhandledRequest: 'bypass',
+    // }); // Node.js 환경에서 Server 활성화
   } else {
     const { worker } = await import('./browser');
-    worker.start(); // 브라우저 환경에서 Worker 활성화
+    worker.start({
+      onUnhandledRequest: 'bypass',
+    }); // 브라우저 환경에서 Worker 활성화
   }
 }
 export { initMSW };
