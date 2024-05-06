f57869e7f6f9502141959afa45797fcc333d77ae
이가영 <tp06100@naver.com>
2024-05-07 00:00:56 +0900

    style : 채팅 페이지 container스타일 변경

--- src/components/Chat/ChatDefault.tsx
+++ src/components/Chat/ChatDefault.tsx
@@ -1,5 +1,31 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.white};
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 30px 120px 0px;
+  position: relative;
+  overflow-y: auto;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: ${(props) => props.theme.secondary1};
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background: ${(props) => props.theme.secondary3};
+  }
+
+  position: relative;
+`;
 const ChatDefault = () => {
-  return <div>f</div>;
+  return <Container>f</Container>;
 };
 
 export default ChatDefault;
--- src/components/GuideBox/GuideBox.tsx
+++ src/components/GuideBox/GuideBox.tsx
@@ -35,7 +35,7 @@ const LawHeader = styled.div`
   align-items: center;
   width: 100%;
   justify-content: start;
-  margin-bottom: 15px;
+  margin-bottom: 10px;
 `;
 
 const LawMainText = styled.div`
@@ -48,20 +48,20 @@ const LawMainText = styled.div`
 
 const LawDetailText = styled.div`
   color: #64748b;
-  font-size: 18px;
+  font-size: 17px;
   font-weight: 400;
-  line-height: 1.3;
   text-align: start;
 `;
 
 const LawGuideContainer = styled.div`
   width: 100%;
   flex: 1;
-  overflow-y: auto;
+  overflow: hidden;
   padding: 30px 120px 0px;
 
   ${breakpoints.md} {
     padding: 30px 40px 0px;
+    font-size: 14px;
   }
   ${breakpoints.sm} {
     padding: 30px 20px 0px;
@@ -82,28 +82,6 @@ const GuideBox = () => {
             안녕하세요. 저는 최근 보이스피싱 피해를 입었습니다. 사기범의 속임수에 넘어가 실수로 2천만원을 송금한
             상황입니다.이러한 상황에서 저는 어떻게 해야 할까요? 이런 종류의 사기를 경찰에 신고하는 정확한 절차와 필요한
             서류에 대해서도 안내받고 싶습니다.
-          </LawDetailText>
-        </LawContainer>
-        <LawContainer>
-          <LawHeader>
-            <Icon icon="car" fill="primary" size="40" />
-            <LawMainText>교통사고</LawMainText>
-          </LawHeader>
-          <LawDetailText>
-            안녕하세요. 최근 교통사고를 당해 상당한 피해를 입었습니다. 신호 대기 중 후방에서 상대방 차량에 의해 추돌
-            당했는데 법적인 조치에 대한 조언을 구하고자 합니다. 또한 이에 대한 상대방의 책임 여부와 보상 절차에 대해
-            자세히 알고 싶습니다.
-          </LawDetailText>
-        </LawContainer>
-        <LawContainer>
-          <LawHeader>
-            <Icon icon="car" fill="primary" size="40" />
-            <LawMainText>교통사고</LawMainText>
-          </LawHeader>
-          <LawDetailText>
-            안녕하세요. 최근 교통사고를 당해 상당한 피해를 입었습니다. 신호 대기 중 후방에서 상대방 차량에 의해 추돌
-            당했는데 법적인 조치에 대한 조언을 구하고자 합니다. 또한 이에 대한 상대방의 책임 여부와 보상 절차에 대해
-            자세히 알고 싶습니다.
           </LawDetailText>
         </LawContainer>
         <LawContainer>
