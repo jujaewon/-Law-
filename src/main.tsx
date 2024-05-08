e45e1a7d262f92bf5a21ca42702491acabffd9d8
이가영 <tp06100@naver.com>
2024-05-08 13:38:57 +0900

    fix : useEffect내에서 msw호출시 오류 발생 고침

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import { CookiesProvider } from 'react-cookie';
 
-import { ThemeProvider } from '@emotion/react';
 import ReactDOM from 'react-dom/client';
-import ModalsProvider from '@components/Modal/manage/ModalsProvider';
-import GlobalStyle from '@styles/global';
-import theme from '@styles/theme';
+
 import { initMSW } from '@/mocks/worker';
+
 import App from './App';
 import './index.css';
 
-if (import.meta.env.VITE_DEV) {
-  initMSW();
+async function prepare() {
+  if (import.meta.env.VITE_DEV) {
+    await initMSW();
+    console.log('App prepared');
+  }
+  return await new Promise((resolve) => setTimeout(resolve, 2000));
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <CookiesProvider>
-        <ModalsProvider>
-          <App />
-        </ModalsProvider>
-      </CookiesProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
-);
+prepare().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
