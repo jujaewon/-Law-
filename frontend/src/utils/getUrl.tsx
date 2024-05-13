e8141427a8bb2f7064bd8be0c7ffea489d8ff971
이가영 <tp06100@naver.com>
2024-05-13 15:29:04 +0900

    fix : 로그인 url 변경

--- frontend/src/utils/getUrl.tsx
+++ frontend/src/utils/getUrl.tsx
@@ -2,6 +2,7 @@ const getUrl = (type: string) => {
   if (type === 'dev') return 'http://localhost:8082';
   if (type === 'test') return 'http://msw:8080';
   if (type === 'real') return 'https://test.hellolaw.kr:8000';
+  if (type === 'real-auth') return 'https://test.hellolaw.kr';
   if (type === 'auth') return 'http://localhost:8099';
   return 'http://msw:8080';
 };
