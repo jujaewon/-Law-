6182bde91c13a1a088108111ffc6512d3a1b02e7
이경민 <kmeye0419@gmail.com>
2024-05-13 14:33:11 +0900

    feat: AI 서버 데이터 요청 class 합침, request, response model 문제 해결

--- hellolaw/src/main/java/com/hellolaw/hellolaw/internal/service/SuggestionService.java
+++
@@ -1,9 +0,0 @@
-package com.hellolaw.hellolaw.internal.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.hellolaw.hellolaw.internal.dto.SuggestionDto;
-
-public interface SuggestionService {
-
-	SuggestionDto getSuggestion(String question) throws JsonProcessingException;
-}
