97c92ece453fddc00ed2f7be0df75d6d3a286b0e
이경민 <kmeye0419@gmail.com>
2024-05-16 10:24:38 +0900

    feat: Category response 변경

--- hellolaw/src/main/java/com/hellolaw/hellolaw/dto/QuestionHistoryResponse.java
+++ hellolaw/src/main/java/com/hellolaw/hellolaw/dto/QuestionHistoryResponse.java
@@ -2,7 +2,7 @@ package com.hellolaw.hellolaw.dto;
 
 import com.hellolaw.hellolaw.entity.Answer;
 import com.hellolaw.hellolaw.entity.Question;
-import com.hellolaw.hellolaw.util.CategoryConstant;
+
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -11,28 +11,29 @@ import lombok.extern.slf4j.Slf4j;
 @Builder
 @Slf4j
 public class QuestionHistoryResponse {
-    private Long questionId;
-    private String summary;
-    private String lawType;
-    private String category;
+	private Long questionId;
+	private String summary;
+	private String lawType;
+	private String category;
 
+	public static QuestionHistoryResponse createQuestionHistoryResponse(Question question) {
 
-    public static QuestionHistoryResponse createQuestionHistoryResponse(Question question) {
+		Answer answer = question.getAnswer();
+		log.info("history-answer : " + answer);
 
-        Answer answer = question.getAnswer();
-        log.info("history-answer : " + answer);
+		//answer이 없을때
+		if (answer == null || answer.getRelatedAnswers().isEmpty()) {
+			log.info("answer is null");
+			return null;
+		}
 
-        //answer이 없을때
-        if (answer == null || answer.getRelatedAnswers().isEmpty()) {
-            log.info("answer is null");
-            return null;
-        }
-        return QuestionHistoryResponse.builder()
-                .questionId(question.getId())
-                .summary(question.getContents())
-                .lawType(answer.getRelatedAnswers().get(0).getPrecedent().getCaseName())
-                .category(CategoryConstant.getCategoryInKorean(answer.getRelatedAnswers().get(0).getLaw().getCategory().name()))
-                .build();
+		String category = question.getCategory() == null ? "기타" : question.getCategory();
+		return QuestionHistoryResponse.builder()
+			.questionId(question.getId())
+			.summary(question.getContents())
+			.lawType(answer.getRelatedAnswers().get(0).getPrecedent().getCaseName())
+			.category(category)
+			.build();
 
-    }
+	}
 }
