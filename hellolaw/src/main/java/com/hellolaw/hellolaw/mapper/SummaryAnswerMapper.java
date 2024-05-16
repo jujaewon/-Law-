b92b4d13761ed430fdccc222c27e50f5429a826e
이경민 <kmeye0419@gmail.com>
2024-05-16 09:55:03 +0900

    feat: SummaryAnswerMapper Mapping 추가

--- hellolaw/src/main/java/com/hellolaw/hellolaw/mapper/SummaryAnswerMapper.java
+++ hellolaw/src/main/java/com/hellolaw/hellolaw/mapper/SummaryAnswerMapper.java
@@ -1,6 +1,7 @@
 package com.hellolaw.hellolaw.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.hellolaw.hellolaw.entity.Answer;
 import com.hellolaw.hellolaw.entity.SummaryAnswer;
@@ -8,5 +9,7 @@ import com.hellolaw.hellolaw.entity.SummaryAnswer;
 @Mapper(componentModel = "spring")
 public interface SummaryAnswerMapper {
 
+	@Mapping(target = "contents", source = "contents")
+	@Mapping(target = "answer", source = "answer")
 	SummaryAnswer toSummaryAnswer(String contents, Answer answer);
 }
