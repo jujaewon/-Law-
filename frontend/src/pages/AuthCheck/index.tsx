1704326431167dbc704b06e379d34f442392b88b
박우철 <woochul102@gmail.com>
2024-05-17 17:00:27 +0900

    Update index.tsx

--- frontend/src/pages/AuthCheck/index.tsx
+++ frontend/src/pages/AuthCheck/index.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 const AuthCheck = () => {
   const handleKaKaoLogin = () => {
     console.log('구글 로그인 진행중');
-    const env = import.meta.env.VITE_DEV;
+    const env = import.meta.env.VITE_ENV || import.meta.env.VITE_DEV;
     console.log('환경', env);
     const params = new URLSearchParams(location.search);
     const type = params.get('type');
